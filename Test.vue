e65742a27096c0a9e8e0b7ffb6d6b7234bf855fb
SaekiRaku <ThomasAntiSoloist@yeah.net>
2017-05-14 17:06:46 +0800

    Create Test.vue

---
+++ Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Hello World</p>
+    <slot>
+    </slot>
+  </div>
+<template>
+
+<style lang="less" scoped>
+  div {
+    background:#03A9F4;
+  }
+</style>
+
+<script>
+  export default {
+    data(){
+      return {
+        temp:0;
+      }
+    }
+  }
+</script>
